3e2eccc2ab130758100f05381d39999f63740db3
Trevor Mackin <tismetrm@gmail.com>
2021-05-05 18:39:52 -0700

    Test security and make appropriate updates

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -16,30 +16,30 @@ router.post("/users/register",
             { username: req.body.username },
             async function (err, foundUser) {
                 if (err) console.log(err);
+                //* If there is no user, create a new user
+                if (!foundUser) {
+                    console.log(chalk.blue.bold("!foundUser"))
+                    const user = new User({ username: req.body.username });
+                    await user.setPassword(req.body.password);
+                    user.localAccount = true;
+                    await user.save();
+                    passport.authenticate("local")(req, res, function () {
+                        res.send(req.user);
+                    });
+                }
+                //* If there is a user through OAUTH, but no local account and password, set one
+                else if (foundUser && !foundUser.localAccount) {
+                    console.log(chalk.red.bold("foundUser && !foundUser.localAccount"));
+                    await foundUser.setPassword(req.body.password);
+                    foundUser.localAccount = true;
+                    await foundUser.save();
+                    passport.authenticate("local")(req, res, function () {
+                        res.send(req.user);
+                    });
+                }
                 else {
-                    if (!foundUser) {
-                        const user = new User({ username: req.body.username });
-                        await user.setPassword(req.body.password);
-                        user.localAccount = true;
-                        await user.save();
-                        passport.authenticate("local")(req, res, function () {
-                            res.send(req.user);
-                        });
-                    } else {
-                        // if a user exists through alternate OAuth, but no local strategy or password exists on user entry
-                        if (!foundUser.localAccount) {
-                            await foundUser.setPassword(req.body.password);
-                            // (NOTES: @AUTHROUTES 001)
-                            foundUser.localAccount = true;
-                            await foundUser.save();
-                            passport.authenticate("local")(req, res, function () {
-                                res.send(req.user);
-                            });
-                        } else {
-                            console.log("This account already exists!");
-                            res.send({ message: "This account already exists!" })
-                        }
-                    }
+                    console.log(chalk.blue("This account already exist"));
+                    res.send({error: "This account already exists!"})
                 }
             }
         );
@@ -108,6 +108,26 @@ router.get("/userdata/:userId/", function (req, res) {
     });
 });
 
+//* UPDATE (update any combination of fields in an entry. Use PUT for updating all fields.)
+router.put("/users/edit/:id", function (req, res) {
+    console.log(req.body);
+    console.log("In put");
+    User.updateOne(
+        { _id: req.body._id },
+        // {_id: req.params.id},
+        //! This "$set" will automatically update only the fields that have values from req.body
+        { $set: req.body },
+        function (err) {
+            if (!err) {
+                res.send("Successfully updated the user!");
+            } else {
+                res.send(err);
+            }
+        }
+    )
+}
+)
+
 // ! Logout (end session)
 router.get("/logout",
     function (req, res) {
@@ -129,6 +149,55 @@ module.exports = router;
 
 
 
+// router.post("/users/register",
+//     function (req, res) {
+//         User.findOne(
+//             { username: req.body.username },
+//             async function (err, foundUser) {
+//                 if (err) console.log(err);
+//                 else {
+//                     if (!foundUser) {
+//                         const user = new User({ username: req.body.username });
+//                         await user.setPassword(req.body.password);
+//                         user.localAccount = true;
+//                         await user.save();
+//                         passport.authenticate("local")(req, res, function () {
+//                             res.send(req.user);
+//                         });
+//                     } else {
+//                         // if a user exists through alternate OAuth, but no local strategy or password exists on user entry
+//                         if (!foundUser.localAccount) {
+//                             await foundUser.setPassword(req.body.password);
+//                             // (NOTES: @AUTHROUTES 001)
+//                             foundUser.localAccount = true;
+//                             await foundUser.save();
+//                             passport.authenticate("local")(req, res, function () {
+//                                 res.send(req.user);
+//                             });
+//                         } else {
+//                             console.log("This account already exists!");
+//                             res.send(err);
+//                             // req.login(foundUser, function (err) {
+//                             //     if (err) {
+//                             //         console.log(err);
+//                             //     } else {
+//                             //         passport.authenticate("local")(req, res, function () {
+//                             //             res.send(req.user);
+//                             //         });
+//                             //     }
+//                             // });
+//                         }
+//                     }
+//                 }
+//             }
+//         );
+//     }
+// );
+
+
+
+
+
 
 
 //! REFERENCE NOTES BELOW
@@ -139,3 +208,14 @@ module.exports = router;
 NOTE: "localAccount" is a Boolean property on the userSchema. Because users may first gain access through alternate OAuth methods (google, fb, etc.), I needed a way to check if the user currently registering "locally", had already registered locally before. Structuring the schema with this additional Boolean allowed me greater control when writing the conditional statements. This helped to prevent someone from registering a second time on an existing account (previously created through alt OAuth), which would have resulted in overwriting any previous passwords.
 That said, I was originally faced with this issue after having trouble running conditional statements on any database field values that were security related (ex: password, salt, hash). This is my reasoning behind creating an additional Boolean field-localAccount- that would keep track of whether or not a local strategy already exists on a user entry.
 */
+
+
+
+
+
+
+
+
+
+
+
