ddcfe9dc1ef2e8e95c49733f19c6e6ea460c7030
Trevor Mackin <tismetrm@gmail.com>
2021-06-22 16:55:47 -0700

    Testing content security policy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "script-src": ["'self'", "'unsafe-inline'", "'sha256-EEmuAbOJyAdZM+wSTy8jvfjugtY8gKfcRXH4HekPE+4='"],
+      "script-src": ["'sha256-EEmuAbOJyAdZM+wSTy8jvfjugtY8gKfcRXH4HekPE+4='"],
       "style-src": ["'self'", "'unsafe-inline'" ],
     },
   })
