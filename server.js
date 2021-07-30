c352af2618bba41fdd72bcfb3aa9fefa715e470d
Trevor Mackin <tismetrm@gmail.com>
2021-07-30 12:29:07 -0700

    Testing  Content Securtiy Policy script-src

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
-      "script-src": ["'https://mern-stack-authentication.herokuapp.com'", "'sha256-EEmuAbOJyAdZM+wSTy8jvfjugtY8gKfcRXH4HekPE+4='"],
+      "script-src": ["'self'", "'sha256-EEmuAbOJyAdZM+wSTy8jvfjugtY8gKfcRXH4HekPE+4='"],
      // "style-src": ["'self'", "'unsafe-inline'"],
     },
   })
